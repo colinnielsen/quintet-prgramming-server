376f3338ba9a8af5a3608aa673f15437f114d688
Administrator <adriandlai@gmail.com>
2018-05-25 13:29:16 -0600

    added cors stuff

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ function notFound(req, res, next) {
     res.status(404).send({error: 'Url not found', status: 404, url})
   }
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use("/jokes", jokes);
 
